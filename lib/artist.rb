4fff606bc56882475a1846c84a91079937556ca4
Pancho Labrador <plabradorjr@gmail.com>
2019-10-31 11:45:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,19 @@
 class Artist
 
-  attr_accessor
+  attr_accessor :name
 
   @@all = []
 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+
   def self.all
     @@all
-  end 
+  end
 end
